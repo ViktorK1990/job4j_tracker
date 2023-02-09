e6de35cf25086d274f40e1be8ad0fd31e70770df
viktorK1990 <korotchinV1990@yandex.ru>
2023-02-09 22:57:33 +0800

    Перегрузка конструктора

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -3,6 +3,16 @@ package ru.job4j.tracker;
 public class Item {
     private int id;
     private String name;
+    public Item(){
+
+    }
+    public Item(String name){
+        this.name = name;
+    }
+    public Item(int id, String name){
+        this.name = name;
+        this.id = id;
+    }
 
     public int getId() {
         return id;

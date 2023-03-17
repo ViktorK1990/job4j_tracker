204209387f79dbdd64d01dc87215918468fea892
viktorK1990 <korotchinV1990@yandex.ru>
2023-03-17 11:39:25 +0800

    Set, HashSet, Iterator

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -2,9 +2,10 @@ package ru.job4j.tracker;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Tracker {
-    private final ArrayList <Item> items = new ArrayList<>();
+    private final List<Item> items = new ArrayList<>();
     private int ids = 1;
 
 
